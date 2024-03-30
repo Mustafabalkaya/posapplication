7dfee4c935da6d38528cf9009ce0e7bd5aeea818
Mustafabalkaya <balkayamustafa99@gmail.com>
2024-03-30 16:45:19 +0300

    gtrg

--- client/src/pages/auth/Login.jsx
+++ client/src/pages/auth/Login.jsx
@@ -2,7 +2,6 @@ import { Button, Carousel, Checkbox, Form, Input, message } from "antd";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import AuthCarousel from "../../components/auth/AuthCarousel";
-import "./style.css";
 
 const Login = () => {
   const navigate = useNavigate();
@@ -43,7 +42,7 @@ const Login = () => {
   };
 
   return (
-    <div className="h-screen overflow-hidden"style={{ height: "100vh" }}>
+    <div className="h-screen">
       <div className="flex justify-between h-full">
         <div className="xl:px-20 px-10 w-full flex flex-col h-full justify-center relative">
           <h1 className="text-center text-5xl font-bold mb-2">LOGO</h1>
@@ -96,10 +95,12 @@ const Login = () => {
               </Button>
             </Form.Item>
           </Form>
+          <div className="flex justify-center absolute left-0 bottom-10 w-full">
             Henüz bir hesabınız yok mu?&nbsp;
             <Link to="/register" className="text-blue-600">
               Şimdi kaydol
             </Link>
+          </div>
         </div>
         <div className="xl:w-4/6 lg:w-3/5 md:w-1/2 md:flex hidden bg-[#6c63ff] h-full">
           <div className="w-full h-full flex items-center">
